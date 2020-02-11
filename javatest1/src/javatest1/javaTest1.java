a6cb0c2dceae17f1bf16ca3f0b5dd29fb92d2bef
Necro <ghostskadi@gmail.com>
2020-02-11 19:09:24 +0200

    TT javaTest1

---
+++ javatest1/src/javatest1/javaTest1.java
@@ -0,0 +1,13 @@
+package javatest1;
+
+public class javaTest1 {
+	public static void main(String[] args) {
+
+//		//System.out.println("Hello World!!");
+//		
+//		int a = 2;
+//		long b = 3;
+//		
+		//javaTest1
+	}
+}
